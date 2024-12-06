9d598d4f4ef940c9788ebd96caad0bad06507202
cesar_ramirez <cesar_ramirez@hacienda.gov.py>
2024-12-06 16:09:55 -0300

    se agrega funcinoalidad para agregar foto de usuario

--- src/app/modules/users/users-add/users-add.component.ts
+++ src/app/modules/users/users-add/users-add.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-users-add',
   templateUrl: './users-add.component.html',
   styleUrls: ['./users-add.component.scss']
 })
-export class UsersAddComponent {
+export class UsersAddComponent implements OnInit {
+
+  rol: string= ''
+  name: string= ''
+  surname: string= ''
+  email: string= ''
+  password: string= ''
+  profesion: string= ''
+  description: string= ''
+
+  FILE_AVATAR: any
+  IMAGEN_PREVISUALIZAR: any= 'assets/media/avatars/300-6.jpg'
+
+  constructor(public toaster: ToastrService){ }
+
+  ngOnInit(): void {
+  }
+
+  procesarAvatar($event: any) {
+    if($event.target.files[0].type.indexOf("image") < 0){
+      this.toaster.error('Solamente se aceptan imagenes', 'Validaciones')
+      return
+    }
+    this.FILE_AVATAR = $event.target.files[0]
+    let reader = new FileReader()
+    reader.readAsDataURL(this.FILE_AVATAR)
+    reader.onloadend = () => this.IMAGEN_PREVISUALIZAR = reader.result;
+  }
+
+  removeImage() {
+    this.IMAGEN_PREVISUALIZAR = 'assets/media/avatars/300-6.jpg'
+  }
 
 }
