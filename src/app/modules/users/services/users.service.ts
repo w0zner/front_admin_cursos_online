625a43a57bd4ebb79b3dd590429d704c5a5e9458
Rodrigo Ramirez <cesarrodrigoramirez@gmail.com>
2024-12-11 22:47:52 -0300

    add services functions

--- src/app/modules/users/services/users.service.ts
+++ src/app/modules/users/services/users.service.ts
@@ -1,9 +1,75 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, finalize, Observable } from 'rxjs';
+import { AuthService } from '../../auth';
+import { URL_SERVICIOS } from 'src/app/config/config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  isLoading$: Observable<boolean>;
+  isLoadingSubject: BehaviorSubject<boolean>;
+
+  constructor(
+    private http: HttpClient,
+    public authservice: AuthService,
+  ) {
+    this.isLoadingSubject = new BehaviorSubject<boolean>(false);
+    this.isLoading$ = this.isLoadingSubject.asObservable();
+  }
+
+  listUsers(search: any = null) {
+    this.isLoadingSubject.next(true)
+    const headers = new HttpHeaders({'token': this.authservice.token})
+
+    let link = '?T='
+    if(search) {
+      link = '&search=' + search
+    }
+
+    const url = URL_SERVICIOS + '/users/list' + link
+
+    return this.http.get(url, {headers: headers}).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    )
+  }
+
+  register(data: any){
+    this.isLoadingSubject.next(true)
+    const headers = new HttpHeaders({'token': this.authservice.token})
+
+    const url = URL_SERVICIOS + '/users/register-admin'
+
+    return this.http.post(url, data,{headers: headers}).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    )
+  }
+
+  update(data: any){
+    this.isLoadingSubject.next(true)
+    const headers = new HttpHeaders({'token': this.authservice.token})
+
+    const url = URL_SERVICIOS + '/users/update'
+
+    return this.http.post(url, data,{headers: headers}).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    )
+  }
+
+
+  delete(id: any){
+    this.isLoadingSubject.next(true)
+    const headers = new HttpHeaders({'token': this.authservice.token})
+
+    const url = URL_SERVICIOS + '/users/delete/' + id
+
+    return this.http.delete(url, {headers: headers}).pipe(
+      finalize(() => this.isLoadingSubject.next(false))
+    )
+  }
+
+
+
 }
--- src/app/modules/users/users-list/users-list.component.ts
+++ src/app/modules/users/users-list/users-list.component.ts
@@ -1,20 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { UsersAddComponent } from '../users-add/users-add.component';
+import { UsersService } from '../services/users.service';
+
+interface Usuario {
+  _id: string;
+  name: string;
+  surname: string;
+  email: string;
+  rol: string;
+  avatar: string;
+}
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.scss']
 })
+
+
 export class UsersListComponent implements OnInit {
 
-  constructor(public modalService: NgbModal) {
+  users: Usuario[] = []
+
+  constructor(public modalService: NgbModal, private userService: UsersService) {
 
   }
 
   ngOnInit(): void {
+    this.userService.listUsers().subscribe({
+      next: (response: any) => {
 
+        this.users = response as Usuario[]
+        console.log(this.users)
+      },
+      error: (err) => {
+        console.error(err)
+      }
+    })
   }
 
   registerUser() {
